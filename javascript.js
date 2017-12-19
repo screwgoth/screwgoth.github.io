090f2fbf7f100711b3fb41f98a75f7490418ff0f
Raseel Bhagat <raseelbhagat@gmail.com>
2017-12-19 11:42:12 +0530

    Updated personal details

--- javascript.js
+++ javascript.js
@@ -71,51 +71,57 @@ $('body').terminal(function(cmd, term) {
   var highLight = function(text){
     return "[[iu;#90ca9a;]" + text + "]";
   }
-  var whoami = "[[ib;#d0d4e5;]Hello world!, my name is  ]"+highLight("Manish Kumar,")
-  +"[[;#d0d4e5;]A designer, developer, gamer, engineer, loser and ...I don't know. May be I'm not an expert in any particular field but I just do things.]";
 
   if (cmd == 'help') {
       term.echo(commandText("   contact")+"          display contact infomation");
       term.echo(commandText("   whoami")+"           display my short brief");
       term.echo(commandText("   clear")+"            clear terminal text");
-      term.echo(commandText("   address")+"          display address infomation");
+      term.echo(commandText("   home")+"             display address infomation");
+      term.echo(commandText("   workspace")+"        display work place information");
       term.echo(commandText("   about")+"            information about this page");
-      term.echo(commandText("   techskills")+"       display my technical skills");
+      term.echo(commandText("   tech")+"             display my technical skills");
       term.echo(commandText("   age")+"              display my Age");
+      term.echo(commandText("   exit")+"             exit this page");
     }
   else if (cmd=='whoami'){
-      term.echo("[[i;#d0d4e5;]Hello world!, my name is \n]"+highLight("Raseel Bhagat\n")
-      +"[[;#d0d4e5;]A designer, developer, gamer, engineer, loser and ...I don't know. May be I'm not an expert in any particular field but I just do things.]\n"
+      term.echo("[[i;#d0d4e5;]Hello world!, my name is ]"+highLight("Raseel Bhagat\n")
+      +"[[;#d0d4e5;]For more info type [[;#e29194;]web\n"
     );
   }
   else if (cmd == 'age') {
       term.echo("[[i;#d0d4e5;]0b100100 (Years)]");
     }
   else if (cmd == 'about') {
-      term.echo("[[i;#FF9800;] V 0.1\n");
+      term.echo("[[i;#FF9800;] Based on Jquery Terminal emulator, jquery.terminal.js\n");
+      term.echo("[[i;#FF9800;] Page version : v0.1\n");
+      term.echo("[[i;#FF9800;] Disclaimer : This page is heavily inspired from : https://man15h.github.io/\n \n");
     }
   else if (cmd == 'school') {
       term.echo("[[i;#d0d4e5;]KReSIT, IIT Mumbai, India]");
     }
-  else if (cmd == 'address') {
-      term.echo("[[i;#d0d4e5;]@home]");
+  else if (cmd == 'home') {
+      term.echo("[[i;#d0d4e5;]Pune, India ]");
     }
-  else if (cmd == 'ls') {
-      term.echo("[[i;#dd4b39;]/Game of Thrones   /Documents    /Videos   /Games]");
+  else if (cmd == 'workspace') {
+      term.echo("[[i;#d0d4e5;]Zymr Systems Pvt. Ltd. : http://zymr.com\n ]");
     }
+  // else if (cmd == 'ls') {
+  //     term.echo("[[i;#dd4b39;]/Game of Thrones   /Documents    /Videos   /Games]");
+  //   }
   else if (cmd == 'ok') {
       term.echo("Thanks!");
     }
   else if (cmd=='contact'){
     term.echo(listHead("    Get in touch via: \n \n")+"[[i;#dd4b39;]     Email:]"+"          raseelbhagat@gmail.com\n"
-    +"[[i;#55acee;]     Twitter:]"+"        @raseel\n"
-    +"[[i;#dd4b39;]     LinkedIn:]"+"    raseelbhagat\n"
-    +"[[i;#0084ff;]     Facebook:]"+"       raseelbhagat");
+    +"[[i;#55acee;]     Twitter:]"+"        https://twitter.com/raseel\n"
+    +"[[i;#dd4b39;]     LinkedIn:]"+"       https://www.linkedin.com/in/raseelbhagat\n"
+    +"[[i;#0084ff;]     Facebook:]"+"       https://facebook.com/raseelbhagat");
   }
-    else if (cmd == 'techskills') {
+    else if (cmd == 'tech') {
         term.echo(listHead("Skills: \n")+
-      listText("      - AngularJS, C++, CSS3, Django, Flask, Git, HTML5, Java, JavaScript, MongoDB, MySQL, Nlp, NodeJS, php, Python\n")+
-      listText("      - Adobe suits, Android Studio, Ansys Fluent, Ansys, ICEM CFD, Atom, Autodesk Inventor, Creo, MATLAB, MS Office, SolidWorks"));
+      listText("      - DevOps, Cloud Applications, Cloud Security\n")+
+      listText("      - Python, C, C++\n")+
+      listText("      - Openstack, AWS, Azure, VMWare, Docker, Kubernetes"));
       }
     else if (cmd == 'web') {
            var i = 0, size =91;
@@ -139,8 +145,12 @@ $('body').terminal(function(cmd, term) {
                }
            })();
          }
+    else if (cmd == 'exit') {
+        term.echo("Thanks!");
+        javascript:window.close();
+      }
   else{
-    term.echo("[[;#f45957;]this is not a valid command, try 'help' for more information]");
+    term.echo("[[;#f45957;]This is not a real shell, try 'help' for more information]");
   }
 }, {
   name: 'xxx',
@@ -150,7 +160,6 @@ $('body').terminal(function(cmd, term) {
   onInit: function(term) {
       // first question
       var msg = '“Who is John Galt?”                            - Ayn Rand \n \n';
-      var msg = '“Disclaimer : This page is heavily inspired from : https://man15h.github.io/”\n \n';
       typed_message(term, msg, 40, function() {
           // typed_prompt(term, "what's your name:", 100);
       });
